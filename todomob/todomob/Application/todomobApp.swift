ad6b1e9c059ca7df1bc3948fae79679d44fa2324
Stanislav Deynekin <164678325+sivkaburka47@users.noreply.github.com>
2025-05-05 01:56:36 +0700

    Добавлена папка проекта мобильного приложения

---
+++ todomob/todomob/Application/todomobApp.swift
@@ -0,0 +1,17 @@
+//
+//  todomobApp.swift
+//  todomob
+//
+//  Created by Станислав Дейнекин on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct todomobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreenView()
+        }
+    }
+}
---
+++ todomob/todomob/Presentation/Screens/MainScreen/MainScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenView.swift
+//  todomob
+//
+//  Created by Станислав Дейнекин on 05.05.2025.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
